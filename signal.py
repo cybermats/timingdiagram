53fb0412c04f2bbc79c9620a621e5339ef80e02e
Mats Fredriksson <matsf@google.com>
2020-02-14 13:46:41 +0100

    Adding more signals

--- signal.py
+++ signal.py
@@ -1,37 +1,12 @@
 from heapq import heappush, heappop
 import itertools
 
-class SignalHeap():
-  def __init__(self):
-    self.pq = []
-    self.entry_finder = {}
-    self.REMOVED = '<removed-task>'
-    self.counter = itertools.count()
-
-  def add_signal(self, signal, time=0):
-    if signal in self.entry_finder:
-      self.remove_signal(signal)
-    count = next(self.counter)
-    entry = [time, count, signal]
-    self.entry_finder[signal] = entry
-    heappush(self.pq, entry)
-
-  def remove_signal(self, signal):
-    entry = self.entry_finder.pop(signal)
-    entry[-1] = self.REMOVED
-
-  def pop_signal(self):
-    while self.pq:
-      time, count, signal = heappop(self.pq)
-      if signal is not self.REMOVED:
-        del self.entry_finder[signal]
-        return time, signal
-    raise KeyError('pop from an empty priority queue')
-
 class Signal():
   UNDEFINED="UNDEFINED"
   HIGH="HIGH"
   LOW="LOW"
+  IMPEDANCE="IMPEDANCE"
+  DATA="DATA"
 
   def __init__(self, name,
                initial_state=UNDEFINED,
@@ -65,7 +40,7 @@ class FlipSignal(Signal):
       initial_state = Signal.LOW
     if not states:
       states = [Signal.LOW, Signal.HIGH]
-      
+
     self.states = states
     super().__init__(name,
                      initial_state=initial_state,
@@ -111,20 +86,14 @@ class CounterSignal(FlipSignal):
   def tick(self, current_time):
     super().tick(current_time)
     old_state, new_state = super()._flip()
-    
+
     self._save(current_time, new_state)
     return old_state, new_state, None
-    
-    
+
   def context(self, s_name, old_state, new_state, current_time, next_time):
     if old_state == Signal.LOW and new_state == Signal.HIGH:
       return current_time
     return None
-      
-    
-    
-    
-    
 
 class SignalCollection():
   def __init__(self):
@@ -158,3 +127,31 @@ class SignalCollection():
           s.name, old_state, new_state, current_time, next_time)
       if next_dependency_time:
         self.heap.add_signal(dependency, next_dependency_time)
+
+
+class SignalHeap():
+  def __init__(self):
+    self.pq = []
+    self.entry_finder = {}
+    self.REMOVED = '<removed-task>'
+    self.counter = itertools.count()
+
+  def add_signal(self, signal, time=0):
+    if signal in self.entry_finder:
+      self.remove_signal(signal)
+    count = next(self.counter)
+    entry = [time, count, signal]
+    self.entry_finder[signal] = entry
+    heappush(self.pq, entry)
+
+  def remove_signal(self, signal):
+    entry = self.entry_finder.pop(signal)
+    entry[-1] = self.REMOVED
+
+  def pop_signal(self):
+    while self.pq:
+      time, count, signal = heappop(self.pq)
+      if signal is not self.REMOVED:
+        del self.entry_finder[signal]
+        return time, signal
+    raise KeyError('pop from an empty priority queue')
